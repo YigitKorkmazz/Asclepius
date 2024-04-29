16559336fe0f29e97db3fffc98eae67558e94679
Yusuf Yigit Korkmaz <yusufyigitkorkmz@gmail.com>
2024-04-29 23:26:35 +0300

    login fxml is changed

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 public class HelloApplication extends Application {
 @Override
 public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("kuzey.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
