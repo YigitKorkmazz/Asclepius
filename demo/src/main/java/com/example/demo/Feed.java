d3ed1694179cf59bf5260008fe0ea8cc401a5e1d
Kıvanç Kuzey Dikici <kuzey.dikici@hotmail.com>
2024-05-06 23:11:06 +0300

    Return Feed Updated

--- demo/src/main/java/com/example/demo/Feed.java
+++ demo/src/main/java/com/example/demo/Feed.java
@@ -115,5 +115,20 @@ public class Feed{
             ex.printStackTrace();
         }
     }
+    
+    @FXML
+    public void goFeed()
+    {
+        try {
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FeedPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("My Donations");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 }
