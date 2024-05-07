87dedb2187b786240506abcd96f9f865054a93fb
Yusuf Yigit Korkmaz <yusufyigitkorkmz@gmail.com>
2024-05-07 11:34:15 +0300

    Registration with database is setup.

--- demo/src/main/java/com/example/demo/Register.java
+++ demo/src/main/java/com/example/demo/Register.java
@@ -4,12 +4,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class Register implements Initializable {
@@ -35,9 +44,27 @@ public class Register implements Initializable {
     //methods
     @FXML
     public void registerButtonOnAction(ActionEvent e)
-    {
-        //TODO
-        //The information of the user will be stored in database and account will be created.
+        {
+            //TODO*
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://sql11.freesqldatabase.com:3306/sql11703300", "sql11703300", "QHBShewvQg");
+                Statement statement = connection.createStatement();
+                String phoneNumber = phoneNumberField.getText();
+                ResultSet existingUser = statement.executeQuery("SELECT * FROM USER WHERE phone_number = '" + phoneNumber + "'");
+                if (!existingUser.next()) {
+                    statement.executeUpdate("INSERT INTO USER (username, blood_Type, password, phone_number) VALUES ('" + nameField.getText() + "', 'ARH+', '" + passwordField.getText() + "', '" + phoneNumber + "')");
+                    showSuccessAlert();
+                }
+                else
+                {
+                    showErrorAlert();
+                }
+
+                // Close the connection
+                connection.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
     }
 
     @FXML
@@ -49,5 +76,36 @@ public class Register implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> list = FXCollections.observableArrayList("ABRH+","ARH+","BRH+","0RH+","ABRH-","ARH-","BRH-","0RH-");
         bloodTypeDropdown.setItems(list);
+    }
+
+    public void showSuccessAlert()
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText("Look, an Information Dialog");
+        alert.setContentText("Your Account has been created !");
+        alert.setOnCloseRequest(event -> {
+            // Open another page here
+            try {
+                Stage stage = (Stage) registerButton.getScene().getWindow();
+                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+                Scene newScene = new Scene(fxmlLoader.load(), 600, 675);
+                stage.setTitle("Login!");
+                stage.setScene(newScene);
+            } catch (IOException ex) {
+                ex.printStackTrace(); // Handle the exception as appropriate
+            }
+        });
+
+        alert.show();
+    }
+
+    public void showErrorAlert()
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Phone Number Already Exists");
+        alert.setContentText("The phone number you entered is already registered.");
+        alert.show();
     }
 }
