3425bac59ab178a65bd84d40ec1cfa9dea34287a
Yusuf Yigit Korkmaz <yusufyigitkorkmz@gmail.com>
2024-05-11 13:34:31 +0300

    Donation creation page class is created and connected to fxml file

--- demo/src/main/java/com/example/demo/DonationRequestCreation.java
+++ demo/src/main/java/com/example/demo/DonationRequestCreation.java
@@ -1,4 +1,146 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DonationRequestCreation {
+
+    @FXML
+    public Label helloLabel;
+
+    @FXML
+    private Label bloodTypeLabel;
+
+    @FXML
+    public Label cityLabel;
+
+    @FXML
+    private Button bloodTypeChangeButton;
+
+    @FXML
+    private Button cityChangeButton;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    private Button myDonationsButton;
+
+    @FXML
+    private Button myDonationRequestsButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextArea addressArea;
+
+    @FXML
+    public void settingsOnAction()
+    {
+        try{
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Settings.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("Settings");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void goSettingsPage1()
+    {
+        try{
+            Stage stage = (Stage) bloodTypeChangeButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Settings.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("Settings");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void goSettingsPage2()
+    {
+        try{
+            Stage stage = (Stage) cityChangeButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Settings.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("Settings");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void goMyDonationRequests()
+    {
+        try {
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MyDonationRequests.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("My Donation Requests");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void goMyDonations()
+    {
+        try {
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MyDonations.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("My Donations");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void goFeed()
+    {
+        try {
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FeedPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("My Donations");
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize (){
+        User currentUser = Feed.getCurrentUser();
+        if (currentUser != null) {
+            helloLabel.setText("Hello, " + currentUser.getName());
+        } else {
+            helloLabel.setText("Hello, Guest");
+        }
+
+        cityLabel.setText("Ankara");
+    }
 }
